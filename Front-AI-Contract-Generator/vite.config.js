5b1c69bb8249c7d0913e619e8a46e7461187cad5
Andrea Temin <AndreaTemin@gmail.com>
2025-08-25 18:35:35 +0300

    fix - Support (blocked:mixed-content)

--- Front-AI-Contract-Generator/vite.config.js
+++ Front-AI-Contract-Generator/vite.config.js
@@ -8,4 +8,14 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  server: {
+    proxy: {
+      // string shorthand: http://localhost:5173/api -> http://127.0.0.1:8000/api
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
+  }
 })
